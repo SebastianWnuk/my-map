82e21df0ad77d8dc6e42d69d87e883aab7e544d7
SebastianWnuk <76219523+SebastianWnuk@users.noreply.github.com>
2021-07-03 23:32:51 +0000

    Deploying to gh-pages from master @ 1666e952188172583ce9d8b791ad3fa41fec46fc 🚀

--- script.js
+++ script.js
@@ -2,58 +2,19 @@
 console.log("Hallo");
 WA.sendChatMessage('Hallo und Herzlich Willkommen auf unserem Campus :)  ', 'Easter-Eggs-Entwickler');
 
-WA.onEnterZone('myZone', () => {
-    currentPopup =  WA.openPopup("popupRectangle","You can purchase virtual office in WorkAdventure",[
-        {
-            label: "See the pricing",
-            className: "popUpElement",
-            callback: (popup) => {
-                // Close the popup when the "Close" button is pressed.
-                popup.close();
-            }
-        }]);
-    
-})
 
 WA.onLeaveZone('myZone', () => {
     WA.sendChatMessage("Aus der Zone gelaufen", 'Mr Robot');
 })
 
-var zoneOfficeName = "popupOfficeZone";
-var zoneEventName = "popupEventZone";
-var zoneSchoolName = "popupSchoolZone";
-var zoneTCMName = "popupTCMZone";
-
-var urlPricing = "https://workadventu.re/pricing";
-var urlSchoolOffer = "https://workadventu.re/school-offer";
-var urlEvent = "https://workadventu.re/events";
-var currentPopup = undefined;
-
-WA.onEnterZone(zoneOfficeName, () => {
-   currentPopup =  WA.openPopup("popUpOffice","You can purchase virtual office in WorkAdventure",[
-        {
-            label: "See the pricing",
-            className: "popUpElement",
-            callback: (popup => {
-                WA.openTab(urlPricing);
-            })
-        }]);
-})
-
-
-WA.onLeaveZone(zoneSchoolName, closePopUp)
-
-WA.onLeaveZone(zoneTCMName, closePopUp)
-
-WA.onLeaveZone(zoneEventName, closePopUp)
-
-WA.onLeaveZone(zoneOfficeName, closePopUp)
-
-function closePopUp(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
-
-console.log("Hallo");
+WA.onEnterZone('myZone', () => {
+    WA.disablePlayerControls();
+    WA.openPopup("popupRectangle", 'This is an imporant message!', [{
+        label: "Got it!",
+        className: "primary",
+        callback: (popup) => {
+            WA.restorePlayerControls();
+            popup.close();
+        }
+    }]);
+});
