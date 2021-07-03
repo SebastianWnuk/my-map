9bf6cedecd863ff1afb3a2ef1c5dd0751a5d5486
SebastianWnuk <76219523+SebastianWnuk@users.noreply.github.com>
2021-07-03 21:30:59 +0000

    Deploying to gh-pages from master @ 4c13e34c73a29dfab3df7a9e23fa1f2cdfca1e4f 🚀

--- script.js
+++ script.js
@@ -4,10 +4,27 @@ WA.sendChatMessage('Hallo und Herzlich Willkommen auf unserem Campus :)  ', 'Eas
 
 WA.onEnterZone('myZone', () => {
     WA.sendChatMessage("In die Zone gelaufen", 'Mr Robot');
+    
 })
 
 WA.onLeaveZone('myZone', () => {
     WA.sendChatMessage("Aus der Zone gelaufen", 'Mr Robot');
+})
+
+var urlPricing = "https://workadventu.re/pricing";
+var urlSchoolOffer = "https://workadventu.re/school-offer";
+var urlEvent = "https://workadventu.re/events";
+var currentPopup = undefined;
+
+WA.onEnterZone(zoneOfficeName, () => {
+   currentPopup =  WA.openPopup("popUpOffice","You can purchase virtual office in WorkAdventure",[
+        {
+            label: "See the pricing",
+            className: "popUpElement",
+            callback: (popup)=> {
+               popup.close();
+            }
+        }]);
 })
 
 console.log("Hallo");
