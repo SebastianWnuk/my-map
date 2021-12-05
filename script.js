54a135858166d7b02d877580f1952cce212f5b47
SebastianWnuk <76219523+SebastianWnuk@users.noreply.github.com>
2021-12-05 17:57:29 +0000

    Deploying to gh-pages from master @ f4e39980c9d111de03a7c50af5cf5862f89a9403 🚀

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ WA.onInit().then(() => {
 WA.player.onPlayerMove(console.log);
 
 
+
 WA.onEnterZone('myZone', () => {
     
     WA.openPopup('popupRectangle', 'This is an imporant message!', [{
@@ -23,7 +24,28 @@ WA.onEnterZone('myZone', () => {
     }]);
 });
 
+//WA.ui.openPopup(targetObject: string, message: string, buttons: ButtonDescriptor[]): Popup
 
+let helloWorldPopup;
+
+// // Open the popup when we enter a given zone
+// helloWorldPopup = WA.room.onEnterLayer("myZone").subscribe(() => {
+//     WA.ui.openPopup("popupRectangle", 'Hello world!', [{
+//         label: "Close",
+//         className: "primary",
+//         callback: (popup) => {
+//             // Close the popup when the "Close" button is pressed.
+//             popup.close();
+//         }
+//     }]);
+// });
+
+// Close the popup when we leave the zone.
+WA.room.onLeaveLayer("myZone").subscribe(() => {
+    helloWorldPopup.close();
+})
+let hanSoloPopUp;
+hanSoloPopUp= 
 WA.onEnterZone('HanSolozone', () => {
 WA.openPopup('HanSolo', 'Han Solo ist eine fiktive Figur aus der Star Wars- Reihe von George Lucas . Han Solo ist ein rücksichtsloser Schmuggler mit einem sarkastischen Witz und wird von  Harrison Ford gespielt.', [{
     label: "Got it!",
@@ -34,6 +56,10 @@ WA.openPopup('HanSolo', 'Han Solo ist eine fiktive Figur aus der Star Wars- Reih
     }
 }]);
 });
+
+WA.room.onLeaveLayer("mypopupRectangleZone").subscribe(() => {
+    helloWorldPopup.close();
+})
 
 
 
