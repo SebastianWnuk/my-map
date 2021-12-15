2aafba98e6e118c886762aa92cf791a84dac5957
sewn0001 <sewn0001@stud.hs-kl.de>
2021-12-15 17:52:23 +0100

    repo my-map

---
+++ Weihnachtsvorlesung-main/B/Spockl.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Spockl" tilewidth="64" tileheight="64" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="Spock.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="Kirk.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="StarTrek.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="32" source="stormtrooper.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="C3PO.png"/>
+ </tile>
+</tileset>
